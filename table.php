19c77785ad00c81b8d6eb9e9ac185c197f43b85f
Your Name <you@example.com>
2016-10-17 20:39:58 -0400

    hi

--- table.php
+++ table.php
@@ -1,8 +1,8 @@
 <?php
-class Shru {}
-    function Shru($array){
+class html_table {}
+    function html_table($array){
         
-	    $html = '<table>';
+	    $html = '<table border=1>';
 	      
 	          $html .= '<tr>';
 		      foreach($array[0] as $key=>$value){
@@ -11,73 +11,49 @@ class Shru {}
 					      $html .= '</tr>';
 
 					         
-						     foreach( $array as
-						     $key=>$value){
+						     foreach( $array as $key=>$value){
 						             $html .= '<tr>';
-							             foreach($value
-								     as
-								     $key2=>$value2){
-								                 $html
-										 .=
-										 '<td>'
-										 .
-										 $value2
-										 .
-										 '</td>';
+							             foreach($value as $key2=>$value2){
+								                 $html .= '<td>' . $value2 . '</td>';
 										         }
-											         $html
-												 .=
-												 '</tr>';
+											         $html .= '</tr>';
 												     }
 
 												       
 
-												           $html
-													   .=
-													   '</table>';
-													       return
-													       $html;
+												           $html .= '</table>';
+													       return $html;
 													       }
 
-													       $array
-													       =
-													       array(
-													           array('Project_Id'=>'236678','UPDATED_DATE'=>'6/13/2016',
-														   'Week'=>'25',
-														   'Inspector
-														   Name'=>'Bill',
-														   'Status'=>'Pending'),
-														       array('Project_Id'=>'786987','UPDATED_DATE'=>'6/9/2016',
-														       'Week'=>'26',
-														       'Inspector
-														       Name'=>'John',
-														       'Status'=>'Closed'),
-														           array('Project_Id'=>'989165','UPDATED_DATE'=>'6/1/2016',
-															   'Week'=>'25',
-															   'Inspector
-															   Name'=>'Chris',
-															   'Status'=>'Pending'),
-															       array('Project_Id'=>'934210','UPDATED_DATE'=>'7/27/2016',
-															       'Week'=>'30',
-															       'Inspector
-															       Name'=>'Luis',
+
+													       $array = array(
+
+													       array('Project_Id'=>'236678','UPDATED_DATE'=>'6/13/2016',
+													       'Week'=>'25', 'Inspector Name'=>'Bill',
+													       'Status'=>'Pending'),
+													           array('Project_Id'=>'786987','UPDATED_DATE'=>'6/9/2016',
+														   'Week'=>'26', 'Inspector Name'=>'John',
+														   'Status'=>'Closed'),
+														       array('Project_Id'=>'989165','UPDATED_DATE'=>'6/1/2016',
+														       'Week'=>'25', 'Inspector
+														       Name'=>'Chris', 'Status'=>'Pending'),
+														           array('Project_Id'=>'934210','UPDATED_DATE'=>'7/27/2016',
+															   'Week'=>'30', 'Inspector
+															   Name'=>'Luis', 'Status'=>'Open'),
+															       array('Project_Id'=>'989165','UPDATED_DATE'=>'2/1/2016',
+															       'Week'=>'11', 'Inspector
+															       Name'=>'Chris',
 															       'Status'=>'Open'),
-															           array('Project_Id'=>'989165','UPDATED_DATE'=>'2/1/2016',
-																   'Week'=>'11',
-																   'Inspector
-																   Name'=>'Chris',
-																   'Status'=>'Open'),
-																       array('Project_Id'=>'763451','UPDATED_DATE'=>'4/9/2016',
-																       'Week'=>'18',
-																       'Inspector
-																       Name'=>'Bill',
-																       'Status'=>'Pending'),
-																           
+															           array('Project_Id'=>'763451','UPDATED_DATE'=>'4/9/2016',
+																   'Week'=>'18', 'Inspector
+																   Name'=>'Bill',
+																   'Status'=>'Pending'),
 
 
-																	   );
 
-																	   echo
-																	   Shru($array);
+																   );
 
-																	   ?>
+																   echo html_table($array);
+
+																   ?>
+

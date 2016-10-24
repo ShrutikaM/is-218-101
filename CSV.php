2ab4f48e5a86c2746d91b52fa22fcac2fa9e0ec5
Your Name <you@example.com>
2016-10-24 17:39:06 -0400

    CSV

---
+++ CSV.php
@@ -0,0 +1,21 @@
+<?php
+Function readCSV($csvFile){
+    $file_handle = fopen($csvFile, 'r');
+    while (!feof($file_handle)){
+        $line_of_text[] = fgetcsv($file_handle, 1024);
+        }
+        fclose($file_handle);
+        return $line_of_text;
+         
+
+
+}
+
+
+$csvFile = 'carSamples.csv';
+$csv = readCSV($csvFile);
+echo '<pre>';
+print_r($csv);
+echo '<pre>'; 
+
+?>
